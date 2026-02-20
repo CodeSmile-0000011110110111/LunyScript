2eb29930d4c93debfb014a5489cad53da79c92b9
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-20 14:14:20 +0100

    improve demo scene, add monobehaviour equivalent scripts, add logo textures

---
+++ SmokeTests/InputMove/InputTransformMove_LunyScript.cs
@@ -0,0 +1,18 @@
+﻿namespace LunyScript.SmokeTests.Input
+{
+	/// <summary>
+	/// LunyScript implementing a 3-way motion relative to object's orientation
+	/// => 11 lines of code, 327 characters (excluding: empty lines, comments, namespace, usings)
+	/// </summary>
+	public sealed class InputTransformMove_LunyScript : Script
+	{
+		public override void Build(ScriptContext context)
+		{
+			On.FrameUpdate(
+				Transform.MoveBy(Input.Direction("Move"), 4),
+				Transform.MoveUp(Input.Button("Jump").Strength, 4),
+				Transform.MoveDown(Input.Button("Crouch").Strength, 4)
+			);
+		}
+	}
+}
---
+++ SmokeTests/InputMove/InputTransformShift_LunyScript.cs
@@ -0,0 +1,18 @@
+namespace LunyScript.SmokeTests.Input
+{
+	/// <summary>
+	/// LunyScript implementing a 3-way world-axis-aligned motion
+	/// => 11 lines of code, 331 characters (excluding: empty lines, comments, namespace, usings)
+	/// </summary>
+	public sealed class InputTransformShift_LunyScript : Script
+	{
+		public override void Build(ScriptContext context)
+		{
+			On.FrameUpdate(
+				Transform.ShiftBy(Input.Direction("Move"), 4),
+				Transform.ShiftUp(Input.Button("Jump").Strength, 4),
+				Transform.ShiftDown(Input.Button("Crouch").Strength, 4)
+			);
+		}
+	}
+}
