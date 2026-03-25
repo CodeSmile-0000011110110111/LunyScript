70ccf26d446d8914f53107ac073c0684d68db657
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-25 12:14:32 +0100

    variable diagnostics window refactoring

--- Diagnostics/ScriptVariableState.cs
+++ Diagnostics/ScriptVariableState.cs
@@ -8,9 +8,26 @@ namespace LunyScript.Diagnostics
 	/// </summary>
 	internal sealed class ScriptVariableState
 	{
-		public Table.VarHandleBase Handle { get; }
-		public Int32 FrameStamp { get; set; }
+		private readonly Table.VarHandleBase _handle;
 
-		public ScriptVariableState(Table.VarHandleBase handle) => Handle = handle;
+		public String Name => _handle.Name;
+		public Boolean IsConstant => _handle.IsConstant;
+		public Int32 FrameStamp { get; set; }
+		public Int32 ValueTypeOrdinal => _handle is Table.VarHandle h ? (Int32)h.Variable.Type : -1;
+
+		public ScriptVariableState(Table.VarHandleBase handle) => _handle = handle;
+
+		public Boolean HasName(String name) => _handle.Name == name;
+
+		public Boolean Contains(String filterText) => _handle.Name.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) >= 0;
+
+		public Int32 CompareNameTo(ScriptVariableState other) =>
+			String.Compare(_handle.Name, other._handle.Name, StringComparison.OrdinalIgnoreCase);
+
+		public Boolean TryGetVarHandle(out Table.VarHandle varHandle)
+		{
+			varHandle = _handle as Table.VarHandle;
+			return varHandle != null;
+		}
 	}
 }
