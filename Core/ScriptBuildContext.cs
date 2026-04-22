062d28e89cb89981da3d08b2258d059ae378f928
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-22 13:22:35 +0200

    cleanup commit before implementing EngineReferences

--- Core/ScriptBuildContext.cs
+++ Core/ScriptBuildContext.cs
@@ -1,6 +1,4 @@
-﻿using Luny;
-
-namespace LunyScript
+﻿namespace LunyScript
 {
 	/// <summary>
 	/// Contains design-time [Data](xref:LunyScript.ScriptContext.Data) and runtime behaviour [Options](xref:LunyScript.ScriptContext.Options).
@@ -11,9 +9,14 @@ namespace LunyScript
 		/// Runtime behaviour options.
 		/// </summary>
 		public ScriptRuntimeOptions Options;
-		/// <summary>
-		/// Input values and references, for instance: layers, groups, names, design-time values, asset references.
-		/// </summary>
-		public ScriptInspectorData Data { get; internal set; }
+	}
+
+	/// <summary>
+	/// Options that change runtime behaviour of the script.
+	/// </summary>
+	public struct ScriptRuntimeOptions
+	{
+		//public Boolean Singleton { get; set; }
+		//public Boolean PatternMatching { get; set; }
 	}
 }
